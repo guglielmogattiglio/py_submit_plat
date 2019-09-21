d1404f49f206cf1dfef936edeb8b984425b6c285
Guglielmo Gattiglio <g.gattiglio@gmail.com>
2019-09-21 16:05:52 +0200

    add pymysql login data support

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    PYMYSQL_LOGIN_DATA = {'user': os.environ.get('user'), 
+                          'password': os.environ.get('password'), 
+                          'host': os.environ.get('host'), 
+                          'port': int(os.environ.get('port')),
+                          'database': os.environ.get('database')}
